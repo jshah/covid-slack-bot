08c35ebd1d1969e3193c51eb6b84f0a1a96e51bc
Jay Shah <contactjay@gmail.com>
2020-07-03 23:08:37 -0700

    slack response

--- app/controllers/slack_api_controller.rb
+++ app/controllers/slack_api_controller.rb
@@ -3,8 +3,28 @@
 # Handles API events from Slack
 class SlackApiController < ApplicationController
   def covid
-    puts "headers=#{request.headers}"
-    puts "body=#{request.body}"
-    render json: {}, status: :ok
+    puts render_slack_response('CA')
+    render json: render_slack_response('CA'), status: :ok
+  end
+
+  def render_slack_response(text)
+    {
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: text
+          }
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: 'Partly cloudy today and tomorrow'
+          }
+        }
+      ]
+    }.to_json
   end
 end
