8779a681aa3865d8498f8770212e0cef7e547b30
Jay Shah <contactjay@gmail.com>
2020-07-06 22:29:26 -0700

    verify slack requests are authentic

--- app/controllers/slack_api_controller.rb
+++ app/controllers/slack_api_controller.rb
@@ -2,6 +2,7 @@
 
 # Handles API events from Slack
 class SlackApiController < ApplicationController
+  before_action :verify_slack_request_authenticity
   before_action :validate_command
   before_action :validate_text
 
@@ -11,6 +12,27 @@ class SlackApiController < ApplicationController
   end
 
   private
+
+  def gen_hash(secret, data)
+    OpenSSL::HMAC.hexdigest('sha256', secret, data)
+  end
+
+  # https://api.slack.com/authentication/verifying-requests-from-slack
+  def verify_slack_request_authenticity
+    return true if Rails.env.development?
+
+    slack_request_timestamp = request.headers['X-Slack-Request-Timestamp']
+    if (Time.now.to_i - slack_request_timestamp.to_i).abs > 60 * 5
+      return render json: { error: 'Request too old.' }, status: :bad_request
+    end
+
+    sig_basestring = 'v0:' + slack_request_timestamp.to_s + ':' + request.body.read
+    my_signature = 'v0=' + gen_hash(ENV['SLACK_SIGNING_SECRET'], sig_basestring)
+    slack_signature = request.headers['X-Slack-Signature']
+
+    return true if my_signature == slack_signature
+    render json: { error: 'Could not verify slack authenticity.' }, status: :bad_request
+  end
 
   def validate_command
     return if params[:command].present?
--- spec/controllers/slack_api_controller_spec.rb
+++ spec/controllers/slack_api_controller_spec.rb
@@ -3,6 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe SlackApiController do
+  let(:time) { Time.zone.local(2020, 7, 6) }
+
+  def authentic_slack_request
+    headers = { 'X-Slack-Request-Timestamp': time.to_i, 'X-Slack-Signature': 'v0=abc123' }
+    request.headers.merge!(headers)
+    allow(OpenSSL::HMAC).to receive(:hexdigest).and_return('abc123')
+  end
+
+  before do
+    travel_to time
+    authentic_slack_request
+  end
+
   describe 'POST covid_state_data' do
     context 'validations' do
       it 'validates command is present' do
@@ -35,6 +48,22 @@ RSpec.describe SlackApiController do
           ]
         }.to_json
         expect(response.body).to eq(expected)
+      end
+
+      context '#verify_slack_request_authenticity' do
+        it 'renders request too old' do
+          headers = { 'X-Slack-Request-Timestamp': time.to_i - 600 }
+          request.headers.merge!(headers)
+          response = post :covid_state_data, params: {}
+          expect(response.body).to eq({ error: 'Request too old.' }.to_json)
+        end
+
+        it 'renders could not verify slack authenticity' do
+          headers = { 'X-Slack-Request-Timestamp': time.to_i, 'X-Slack-Signature': 'v0=abc124' }
+          request.headers.merge!(headers)
+          response = post :covid_state_data, params: {}
+          expect(response.body).to eq({ error: 'Could not verify slack authenticity.' }.to_json)
+        end
       end
     end
 
